0901099ad80bf7977f2caba046ba27ba282b784b
evfinkn <evan@finken.name>
2023-12-17 23:23:00 -0600

    feat: throw helpful error when argument order is wrong

--- ts/EtoolboxMethods.ts
+++ ts/EtoolboxMethods.ts
@@ -3,12 +3,32 @@ import TexParser from "mathjax-full/js/input/tex/TexParser.js";
 
 import * as EtoolboxUtil from "./EtoolboxUtil.js";
 import { Counter, ETOOLBOX_COUNTER_MAP } from "./EtoolboxUtil.js";
+import TexError from "mathjax-full/js/input/tex/TexError.js";
 
 const EtoolboxMethods: Record<string, ParseMethod> = {};
 
 EtoolboxMethods.NewCounter = function (parser: TexParser, name: string) {
   console.debug("NewCounter");
-  const cs = parser.GetArgument(name);
+  const cs = EtoolboxUtil.GetCsNameArgument(parser, name);
+  // The order has to be \newcounter{cs}[superCounter] like in LaTeX. If `cs` is a "["
+  // then the order is reversed, so we throw an error.
+  // This is different from LaTeX, where an error would be thrown when the counter that
+  // was supposed to be defined is referenced later. However, I figured it'd be helpful
+  // to throw an error here to make the actual error more obvious (since usually,
+  // optional arguments come before required arguments).
+  if (cs === "[") {
+    parser.i--; // Decrement so that GetBrackets() gets the argument correctly.
+    const optionalArg = parser.GetBrackets(name);
+    // It's fine if GetArgument throws an error, since we'd want that regardless
+    // of whether the order is correct or not.
+    const requiredArg = parser.GetArgument(name);
+    throw new TexError(
+      "InvalidArgumentOrder",
+      'Counter name "{%2}" must come before optional argument "[%1]"',
+      optionalArg,
+      requiredArg,
+    );
+  }
   console.debug("cs: ", cs);
   const superCounter = parser.GetBrackets(name);
   console.debug("superCounter: ", superCounter);
