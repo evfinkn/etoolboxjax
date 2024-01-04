2fff4c906e9ba0b853e85ec097a79ec756f2c62c
evfinkn <evan@finken.name>
2024-01-03 20:56:51 -0600

    feat: add better typing to CounterMethods

--- ts/CounterMappings.ts
+++ ts/CounterMappings.ts
@@ -1,25 +1,29 @@
+import type { CommandMapRecord } from "./Util.js";
+
 import { CommandMap } from "mathjax-full/js/input/tex/TokenMap.js";
 
 import CounterMethods from "./CounterMethods.js";
 import { COUNTER_CMD_MAP } from "./CounterUtil.js";
 
+const commands: CommandMapRecord<typeof CounterMethods> = {
+  newcounter: "NewCounter",
+  setcounter: "SetCounter",
+  stepcounter: "StepCounter",
+  addtocounter: "AddToCounter",
+  arabic: ["Format", "toArabic"],
+  roman: ["Format", "toRoman", false],
+  Roman: ["Format", "toRoman", true],
+  alph: ["Format", "toAlph", false],
+  Alph: ["Format", "toAlph", true],
+  fnsymbol: ["Format", "toFnSymbol"],
+  counterwithin: "CounterWithin",
+  counterwithout: "CounterWithout",
+  number: "Number",
+  romannumeral: "RomanNumeral",
+};
+
 export const CounterCommandMap = new CommandMap(
   COUNTER_CMD_MAP,
-  {
-    newcounter: "NewCounter",
-    setcounter: "SetCounter",
-    stepcounter: "StepCounter",
-    addtocounter: "AddToCounter",
-    arabic: ["Format", "toArabic"],
-    roman: ["Format", "toRoman", false],
-    Roman: ["Format", "toRoman", true],
-    alph: ["Format", "toAlph", false],
-    Alph: ["Format", "toAlph", true],
-    fnsymbol: ["Format", "toFnSymbol"],
-    counterwithin: "CounterWithin",
-    counterwithout: "CounterWithout",
-    number: "Number",
-    romannumeral: "RomanNumeral",
-  },
+  commands,
   CounterMethods,
 );
--- ts/CounterMethods.ts
+++ ts/CounterMethods.ts
@@ -6,123 +6,123 @@ import TexError from "mathjax-full/js/input/tex/TexError.js";
 import { Counter, COUNTER_MAP } from "./CounterUtil.js";
 import * as Util from "./Util.js";
 
-const CounterMethods: Record<string, ParseMethod> = {};
+const CounterMethods = {
+  NewCounter(parser: TexParser, name: string) {
+    const cs = Util.GetCsNameArgument(parser, name);
+    // The order has to be \newcounter{cs}[superCounter] like in LaTeX. If `cs` is a "["
+    // then the order is reversed, so we throw an error.
+    if (cs === "[") {
+      throw new TexError(
+        "InvalidArgumentOrder",
+        "Counter name must come before optional argument",
+      );
+    }
+    const superCounter = Util.GetCsNameBrackets(parser, name);
+    new Counter(cs, superCounter);
+    const theCs = `the${cs}`;
+    Util.addMacro(parser, COUNTER_MAP, theCs, Counter.the);
+  },
 
-CounterMethods.NewCounter = function (parser: TexParser, name: string) {
-  const cs = Util.GetCsNameArgument(parser, name);
-  // The order has to be \newcounter{cs}[superCounter] like in LaTeX. If `cs` is a "["
-  // then the order is reversed, so we throw an error.
-  if (cs === "[") {
-    throw new TexError(
-      "InvalidArgumentOrder",
-      "Counter name must come before optional argument",
-    );
-  }
-  const superCounter = Util.GetCsNameBrackets(parser, name);
-  new Counter(cs, superCounter);
-  const theCs = `the${cs}`;
-  Util.addMacro(parser, COUNTER_MAP, theCs, Counter.the);
-};
+  SetCounter(parser: TexParser, name: string) {
+    const counter = Util.GetCounter(parser, name);
+    counter.value = Util.GetNumber(parser, name);
+  },
 
-CounterMethods.SetCounter = function (parser: TexParser, name: string) {
-  const counter = Util.GetCounter(parser, name);
-  counter.value = Util.GetNumber(parser, name);
-};
+  StepCounter(parser: TexParser, name: string) {
+    const counter = Util.GetCounter(parser, name);
+    counter.step();
+  },
 
-CounterMethods.StepCounter = function (parser: TexParser, name: string) {
-  const counter = Util.GetCounter(parser, name);
-  counter.step();
-};
+  AddToCounter(parser: TexParser, name: string) {
+    const counter = Util.GetCounter(parser, name);
+    counter.value += Util.GetNumber(parser, name);
+  },
 
-CounterMethods.AddToCounter = function (parser: TexParser, name: string) {
-  const counter = Util.GetCounter(parser, name);
-  counter.value += Util.GetNumber(parser, name);
-};
+  /**
+   * Handles the `\counterwithin{counter}{superCounter}` command.
+   *
+   * This adds `counter` to `superCounter`'s list of subcounters, meaning that
+   * `counter` will be reset when `superCounter` is changed. The `\thecounter` command
+   * will also be updated to include `\thesuperCounter.` as a prefix, unless the starred
+   * version of `\counterwithin` (i.e. `\counterwithin*`) is used.
+   *
+   * Note that `\counterwithin*` doesn't change the prefix of `\thecounter` if
+   * `superCounter` was already set with `\counterwithin`. If you want to remove the
+   * use `\counterwithout` followed by `\counterwithin*`.
+   *
+   * If `counter` already has a supercounter, it will be removed from that supercounter's
+   * list of subcounters.
+   * @param {TexParser} parser The calling parser.
+   * @param {string} name The name of the calling command.
+   */
+  CounterWithin(parser: TexParser, name: string) {
+    const updateToString = !parser.GetStar();
+    const counter = Util.GetCounter(parser, name);
+    const superCounter = Util.GetCounter(parser, name);
+    counter.within(superCounter, updateToString);
+  },
 
-/**
- * Handles the `\counterwithin{counter}{superCounter}` command.
- *
- * This adds `counter` to `superCounter`'s list of subcounters, meaning that
- * `counter` will be reset when `superCounter` is changed. The `\thecounter` command
- * will also be updated to include `\thesuperCounter.` as a prefix, unless the starred
- * version of `\counterwithin` (i.e. `\counterwithin*`) is used.
- *
- * Note that `\counterwithin*` doesn't change the prefix of `\thecounter` if
- * `superCounter` was already set with `\counterwithin`. If you want to remove the
- * use `\counterwithout` followed by `\counterwithin*`.
- *
- * If `counter` already has a supercounter, it will be removed from that supercounter's
- * list of subcounters.
- * @param {TexParser} parser The calling parser.
- * @param {string} name The name of the calling command.
- */
-CounterMethods.CounterWithin = function (parser: TexParser, name: string) {
-  const updateToString = !parser.GetStar();
-  const counter = Util.GetCounter(parser, name);
-  const superCounter = Util.GetCounter(parser, name);
-  counter.within(superCounter, updateToString);
-};
+  /**
+   * Handles the `\counterwithout{counter}{superCounter}` command.
+   *
+   * This removes `counter` from `superCounter`'s list of subcounters, meaning that
+   * `counter` will no longer be reset when `superCounter` is changed. Additionally, if
+   * `superCounter` was set with the unstarred version of `\counterwithin`, `\thecounter`
+   * will no longer be prefixed with `\thesuperCounter.`.
+   *
+   * If `superCounter` is not `counter`'s supercounter, this command does nothing.
+   * @param {TexParser} parser The calling parser.
+   * @param {string} name The name of the calling command.
+   */
+  CounterWithout(parser: TexParser, name: string) {
+    const counter = Util.GetCounter(parser, name);
+    const superCounter = Util.GetCounter(parser, name);
+    counter.without(superCounter);
+  },
 
-/**
- * Handles the `\counterwithout{counter}{superCounter}` command.
- *
- * This removes `counter` from `superCounter`'s list of subcounters, meaning that
- * `counter` will no longer be reset when `superCounter` is changed. Additionally, if
- * `superCounter` was set with the unstarred version of `\counterwithin`, `\thecounter`
- * will no longer be prefixed with `\thesuperCounter.`.
- *
- * If `superCounter` is not `counter`'s supercounter, this command does nothing.
- * @param {TexParser} parser The calling parser.
- * @param {string} name The name of the calling command.
- */
-CounterMethods.CounterWithout = function (parser: TexParser, name: string) {
-  const counter = Util.GetCounter(parser, name);
-  const superCounter = Util.GetCounter(parser, name);
-  counter.without(superCounter);
-};
+  /**
+   * Handles the formatting-related commands.
+   *
+   * Specifically, this handles the following commands:
+   * - `\arabic`
+   * - `\roman`
+   * - `\Roman`
+   * - `\alph`
+   * - `\Alph`
+   * - `\fnsymbol`
+   *
+   * @param {TexParser} parser The calling parser.
+   * @param {string} name The name of the calling command.
+   * @param {Util.FormatMethod} formatMethod The name of the method to use for
+   *   formatting. Either "toArabic", "toRoman", "toAlph", or "toFnSymbol".
+   * @param {?boolean} capital Whether to capitalize the formatted string.
+   */
+  Format(
+    parser: TexParser,
+    name: string,
+    formatMethod: Util.FormatMethod,
+    capital: boolean | null = null,
+  ) {
+    const counter = Util.GetCounter(parser, name);
+    let formatted = Util[formatMethod](counter.value);
+    formatted = capital ? formatted.toUpperCase() : formatted.toLowerCase();
+    if (/^[a-zA-Z]*$/.test(formatted)) {
+      Util.pushText(parser, formatted);
+    } else {
+      Util.pushMath(parser, formatted);
+    }
+  },
 
-/**
- * Handles the formatting-related commands.
- *
- * Specifically, this handles the following commands:
- * - `\arabic`
- * - `\roman`
- * - `\Roman`
- * - `\alph`
- * - `\Alph`
- * - `\fnsymbol`
- *
- * @param {TexParser} parser The calling parser.
- * @param {string} name The name of the calling command.
- * @param {Util.FormatMethod} formatMethod The name of the method to use for
- *   formatting. Either "toArabic", "toRoman", "toAlph", or "toFnSymbol".
- * @param {?boolean} capital Whether to capitalize the formatted string.
- */
-CounterMethods.Format = function (
-  parser: TexParser,
-  name: string,
-  formatMethod: Util.FormatMethod,
-  capital: boolean | null = null,
-) {
-  const counter = Util.GetCounter(parser, name);
-  let formatted = Util[formatMethod](counter.value);
-  formatted = capital ? formatted.toUpperCase() : formatted.toLowerCase();
-  if (/^[a-zA-Z]*$/.test(formatted)) {
-    Util.pushText(parser, formatted);
-  } else {
-    Util.pushMath(parser, formatted);
-  }
-};
+  Number(parser: TexParser, name: string) {
+    const num = Util.GetNumber(parser, name);
+    Util.pushMath(parser, num.toString());
+  },
 
-CounterMethods.Number = function (parser: TexParser, name: string) {
-  const num = Util.GetNumber(parser, name);
-  Util.pushMath(parser, num.toString());
-};
-
-CounterMethods.RomanNumeral = function (parser: TexParser, name: string) {
-  const num = Util.GetNumber(parser, name);
-  const formatted = Util.toRoman(num);
-  Util.pushText(parser, formatted.toLowerCase());
-};
+  RomanNumeral(parser: TexParser, name: string) {
+    const num = Util.GetNumber(parser, name);
+    const formatted = Util.toRoman(num);
+    Util.pushText(parser, formatted.toLowerCase());
+  },
+} satisfies Record<string, ParseMethod>;
 
 export default CounterMethods;
