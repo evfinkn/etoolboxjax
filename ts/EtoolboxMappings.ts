259f7509c3c551b55b36b5f6d7a698a7b0da5382
evfinkn <evan@finken.name>
2024-01-03 19:23:53 -0600

    feat: add flag methods and type EtoolboxMethods as const

--- ts/EtoolboxMappings.ts
+++ ts/EtoolboxMappings.ts
@@ -1,31 +1,65 @@
+import type TexParser from "mathjax-full/js/input/tex/TexParser.js";
+
 import { CommandMap } from "mathjax-full/js/input/tex/TokenMap.js";
 
 import EtoolboxMethods from "./EtoolboxMethods.js";
 import { COMMAND_MAP } from "./EtoolboxUtil.js";
 
+// prettier-ignore
+type NonParseParams<F> =
+  F extends (parser: TexParser, name: string, ...args: infer R) => any ? R : never;
+
+type EtoolboxNames = keyof typeof EtoolboxMethods;
+type EtoolboxParameters<K> = K extends EtoolboxNames
+  ? K | [K, ...NonParseParams<(typeof EtoolboxMethods)[K]>]
+  : never;
+
+const commands: Record<string, EtoolboxParameters<EtoolboxNames>> = {
+  // TeX flags
+  // NewFlag takes errorIfDefined argument
+  newbool: ["NewFlag", "bool", true],
+  providebool: ["NewFlag", "bool", false],
+  setbool: ["SetFlag", "bool"],
+  // SetFlag accepts value argument
+  booltrue: ["SetFlag", "bool", true],
+  boolfalse: ["SetFlag", "bool", false],
+  // IfFlag takes negate argument
+  ifbool: ["IfFlag", "bool", false],
+  notbool: ["IfFlag", "bool", true],
+  // Toggles
+  newtoggle: ["NewFlag", "toggle", true],
+  providetoggle: ["NewFlag", "toggle", false],
+  settoggle: ["SetFlag", "toggle"],
+  toggletrue: ["SetFlag", "toggle", true],
+  togglefalse: ["SetFlag", "toggle", false],
+  iftoggle: ["IfFlag", "toggle", false],
+  nottoggle: ["IfFlag", "toggle", true],
+  // Macro tests
+  // IfDef takes negate argument
+  ifdef: ["IfDef", false],
+  ifcsdef: ["IfDef", false],
+  ifundef: ["IfDef", true],
+  ifcsundef: ["IfDef", true],
+  // String tests
+  ifstrequal: ["IfStrEqual"],
+  // IfStrEqual takes trim and negate arguments
+  ifstrempty: ["IfBlank", false, false],
+  ifblank: ["IfBlank", true, false],
+  notblank: ["IfBlank", true, true],
+  // Arithmetic tests
+  ifnumcomp: "IfNumComp",
+  ifnumequal: ["IfNumComp", "="],
+  ifnumneq: ["IfNumComp", "!="],
+  ifnumless: ["IfNumComp", "<"],
+  ifnumgreater: ["IfNumComp", ">"],
+  ifnumleq: ["IfNumComp", "<="],
+  ifnumgeq: ["IfNumComp", ">="],
+  ifnumeven: ["IfNumParity", 0],
+  ifnumodd: ["IfNumParity", 1],
+};
+
 export const EtoolboxCommandMap = new CommandMap(
   COMMAND_MAP,
-  {
-    // Macro tests
-    ifdef: ["IfDef", false],
-    ifcsdef: ["IfDef", false],
-    ifundef: ["IfDef", true],
-    ifcsundef: ["IfDef", true],
-    // String tests
-    ifstrequal: "IfStrEqual",
-    ifstrempty: ["IfBlank", false, false],
-    ifblank: ["IfBlank", true, false],
-    notblank: ["IfBlank", true, true],
-    // Arithmetic tests
-    ifnumcomp: "IfNumComp",
-    ifnumequal: ["IfNumComp", "="],
-    ifnumneq: ["IfNumComp", "!="],
-    ifnumless: ["IfNumComp", "<"],
-    ifnumgreater: ["IfNumComp", ">"],
-    ifnumleq: ["IfNumComp", "<="],
-    ifnumgeq: ["IfNumComp", ">="],
-    ifnumeven: ["IfNumParity", 0],
-    ifnumodd: ["IfNumParity", 1],
-  },
+  commands,
   EtoolboxMethods,
 );
--- ts/EtoolboxMethods.ts
+++ ts/EtoolboxMethods.ts
@@ -4,9 +4,8 @@ import type { ParseMethod } from "mathjax-full/js/input/tex/Types.js";
 
 import TexError from "mathjax-full/js/input/tex/TexError.js";
 
+import { Flag } from "./EtoolboxUtil.js";
 import * as Util from "./Util.js";
-
-const EtoolboxMethods: Record<string, ParseMethod> = {};
 
 const handlerTypes: HandlerType[] = [
   "macro",
@@ -14,37 +13,6 @@ const handlerTypes: HandlerType[] = [
   "character",
   "environment",
 ];
-
-EtoolboxMethods.IfDef = function (
-  parser: TexParser,
-  name: string,
-  negate: boolean,
-) {
-  const cs = Util.GetCsNameArgument(parser, name);
-  const handlers = handlerTypes.map((type) =>
-    parser.configuration.handlers.get(type),
-  );
-  const isDefined = handlers.some((handler) => handler.contains(cs));
-  Util.PushConditionsBranch(parser, name, isDefined, negate);
-};
-
-EtoolboxMethods.IfStrEqual = function (parser: TexParser, name: string) {
-  const str1 = parser.GetArgument(name);
-  const str2 = parser.GetArgument(name);
-  Util.PushConditionsBranch(parser, name, str1 === str2);
-};
-
-EtoolboxMethods.IfBlank = function (
-  parser: TexParser,
-  name: string,
-  trim: boolean,
-  negate: boolean,
-) {
-  let str = parser.GetArgument(name);
-  if (trim) str = str.trim();
-
-  Util.PushConditionsBranch(parser, name, str === "", negate);
-};
 
 // Note: \ifnumcomp in etoolbox only supports <, >, and =.
 type RelationSymbol = "=" | "!=" | "<" | ">" | "<=" | ">=";
@@ -57,30 +25,80 @@ const relations: Record<RelationSymbol, (a: number, b: number) => boolean> = {
   ">=": (a, b) => a >= b,
 };
 
-EtoolboxMethods.IfNumComp = function (
-  parser: TexParser,
-  name: string,
-  relationSym?: RelationSymbol,
-) {
-  const num1 = Util.numexpr(parser.GetArgument(name));
-  if (!relationSym) {
-    relationSym = parser.GetArgument(name) as RelationSymbol;
-  }
-  const num2 = Util.numexpr(parser.GetArgument(name));
-  const relation = relations[relationSym];
-  if (!relation) {
-    throw new TexError("InvalidRelation", "Invalid relation: %1", relationSym);
-  }
-  Util.PushConditionsBranch(parser, name, relation(num1, num2));
-};
+const EtoolboxMethods = {
+  // TODO: Flag error messages will be confusing because names will be prefixed with
+  //       "bool-" or "toggle-"
+  NewFlag(
+    parser: TexParser,
+    name: string,
+    prefix: string,
+    errorIfDefined: boolean,
+  ) {
+    const cs = Util.GetCsNameArgument(parser, name);
+    Flag.create(`${prefix}-${cs}`, errorIfDefined);
+  },
 
-EtoolboxMethods.IfNumParity = function (
-  parser: TexParser,
-  name: string,
-  parity: 0 | 1,
-) {
-  const num = Util.numexpr(parser.GetArgument(name));
-  Util.PushConditionsBranch(parser, name, num % 2 === parity);
-};
+  SetFlag(parser: TexParser, name: string, prefix: string, value?: boolean) {
+    const cs = Util.GetCsNameArgument(parser, name);
+    if (value === undefined) {
+      const arg = parser.GetArgument(name);
+      if (arg !== "true" && arg !== "false") {
+        throw new TexError("InvalidFlag", `Invalid boolean value "${arg}"`);
+      }
+      value = arg === "true";
+    }
+    Flag.set(`${prefix}-${cs}`, value);
+  },
+
+  IfFlag(parser: TexParser, name: string, prefix: string, negate: boolean) {
+    const cs = Util.GetCsNameArgument(parser, name);
+    const bool = Flag.get(`${prefix}-${cs}`);
+    Util.PushConditionsBranch(parser, name, bool, negate);
+  },
+
+  IfDef(parser: TexParser, name: string, negate: boolean) {
+    const cs = Util.GetCsNameArgument(parser, name);
+    const handlers = handlerTypes.map((type) =>
+      parser.configuration.handlers.get(type),
+    );
+    const isDefined = handlers.some((handler) => handler.contains(cs));
+    Util.PushConditionsBranch(parser, name, isDefined, negate);
+  },
+
+  IfStrEqual(parser: TexParser, name: string) {
+    const str1 = parser.GetArgument(name);
+    const str2 = parser.GetArgument(name);
+    Util.PushConditionsBranch(parser, name, str1 === str2);
+  },
+
+  IfBlank(parser: TexParser, name: string, trim: boolean, negate: boolean) {
+    let str = parser.GetArgument(name);
+    if (trim) str = str.trim();
+
+    Util.PushConditionsBranch(parser, name, str === "", negate);
+  },
+
+  IfNumComp(parser: TexParser, name: string, relationSym?: RelationSymbol) {
+    const num1 = Util.numexpr(parser.GetArgument(name));
+    if (!relationSym) {
+      relationSym = parser.GetArgument(name) as RelationSymbol;
+    }
+    const num2 = Util.numexpr(parser.GetArgument(name));
+    const relation = relations[relationSym];
+    if (!relation) {
+      throw new TexError(
+        "InvalidRelation",
+        "Invalid relation: %1",
+        relationSym,
+      );
+    }
+    Util.PushConditionsBranch(parser, name, relation(num1, num2));
+  },
+
+  IfNumParity(parser: TexParser, name: string, parity: 0 | 1) {
+    const num = Util.numexpr(parser.GetArgument(name));
+    Util.PushConditionsBranch(parser, name, num % 2 === parity);
+  },
+} satisfies Record<string, ParseMethod>;
 
 export default EtoolboxMethods;
